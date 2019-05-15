8e7bfdda33c336bda4aff1e5ace1eb9a18c96d9a
mikailsenel <mikailsenel@users.noreply.github.com>
2019-05-15 10:25:25 +0300

    Add project files.

---
+++ LCG (Linear Congruential Generators)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LCG__Linear_Congruential_Generators_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
