5f65adbf8571442fef03fe18bef6a7a07375647a
mikailsenel <mikailsenel@users.noreply.github.com>
2019-05-15 10:43:59 +0300

    Success

---
+++ LCG (Linear Congruential Generators)/Generator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCG__Linear_Congruential_Generators_
+{
+    public class Generator
+    {
+        private int a,c,m,z0;
+        public Generator(int multiplier, int increment, int amplitude, int initialValue)
+        {
+            this.a = multiplier;
+            this.c = increment;
+            this.m = amplitude;
+            this.z0 = initialValue;
+        }
+        public double[] LCG_Generators()
+        {
+            /*
+             *      z(k+1)=(a*z(k)+c) mod(m)                a=multiplier
+             *      u(k)=z(k)/m                             c=increment
+             *                                              m=amplitude
+            */
+
+            double[] u = new double[m];
+            double zk = z0;
+            for (int i = 0; i < m; i++)
+            {
+                u[i] = zk / m;
+                zk = (a * zk + c) % m;
+            }
+            return u;
+        }
+    }
+}
--- LCG (Linear Congruential Generators)/Program.cs
+++ LCG (Linear Congruential Generators)/Program.cs
@@ -6,7 +6,18 @@ namespace LCG__Linear_Congruential_Generators_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Generator generator = new Generator(5,3,16,7);
+            double[] randomArray = generator.LCG_Generators();
+
+            Console.WriteLine("Array");
+            foreach (double item in randomArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
+        
+        
+       
     }
 }
